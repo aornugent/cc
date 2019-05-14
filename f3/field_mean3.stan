20442f0b1d195250572cfc213e357a435fd4dea0
Andrew O'Reilly-Nugent <aornugent@gmail.com>
2019-05-14 21:08:45 +1000

    update beta model

---
+++ f3/field_mean3.stan
@@ -0,0 +1,178 @@
+functions {
+  // Eq. 14; Tjorve 2017. 
+  vector gompertz_curve(vector x0, vector xK, 
+                        vector rK, vector t, int n) {
+    
+    // Power operation not vectorised in Stan
+    vector[n] xt = log(xK) + log(x0 ./ xK) .* exp(-rK .* t);
+    
+    return(exp(xt));
+  }
+  
+  // Eq. 7.38; Panik 2014.
+  vector beta_curve(vector x0, vector xK, 
+                    vector tK, vector tmax, 
+                    vector t, int n) {
+    
+    vector[n] log_change = log(1 + (tK - t) ./ (tK - tmax)) + 
+      log(t ./ tK) .* (tK ./ (tK - tmax));
+      
+    // back-transform and cap at 1
+    vector[n] change;
+    for(i in 1:n)
+      change[i] = t[i] > tK[i] ? 1 : exp(log_change[i]);
+    
+    return(x0 + (xK - x0) .* change);
+  }
+}
+data {
+  // sizes
+  int<lower=1> T;       // timespan
+  int<lower=1> N;       // total
+  int<lower=1> N_grp;   // groups
+  int<lower=1> N_trt;   // contrasts
+  int<lower=1> N_plt;   // experiments
+  int<lower=1> N_pop;   // populations
+  int<lower=1> N_meas;  // meas. types
+  int<lower=0> N_mis;   // censored
+  
+  // data
+  vector[N] t;          // time
+  vector[N] y;          // abundance
+  vector[N] gm;         // gap between meas.
+  vector[N_meas] L;     // limits
+  
+  // meas. indices
+  int<lower=1, upper=N> m1[N_plt];
+  int<lower=1, upper=N> m[N - N_plt];
+  int<lower=1, upper=N> m_m1[N - N_plt];
+  int<lower=1, upper=N> m_obs[N - N_mis];
+  int<lower=0, upper=N> m_mis[N_mis];
+  
+  // meas., group, treatment, plot & pop. indices
+  int<lower=1, upper=N_meas> meas[N];
+  int<lower=1, upper=N_grp> grp[N];
+  int<lower=1, upper=N_trt> trt[N];
+  int<lower=1, upper=N_grp> grp_trt[N_trt - N_grp];
+  int<lower=1, upper=N_plt> plt[N];
+  int<lower=1, upper=N_grp> grp_plt[N_plt];
+  int<lower=1, upper=N_pop> pop[N]; 
+  int<lower=1, upper=N_grp> grp_pop[N_pop];
+  
+  // switches
+  int<lower=1, upper=3> model_variant; // varying K, tK or both
+  int<lower=0, upper=1> cen;           // censoring
+}
+parameters{
+  // population intercepts
+  vector[N_pop] log_p0_raw;
+  
+  // carrying capacities
+  vector[N_pop] log_pK_raw;
+  vector[N_grp] log_mu_p;
+  real<lower=0> sigma_log_p;
+  
+  // time that carrying capacity is reached
+  vector<lower=0>[N_pop] tK_raw;
+
+  // time of max growth rate
+  vector<lower=0, upper=1>[N_pop] tmax_raw;
+  real<lower=0> sigma_t;
+
+  // predictors ordered so R- x N_grp at start
+  vector[N_trt - N_grp] log_beta_raw;
+  vector<lower=0>[N_grp] sigma_log_b;
+  
+  // correlation of error
+  vector<lower=0, upper=1>[N_grp] delta;
+  
+  // plot random effects
+  vector[N_plt] log_u_raw;
+  vector<lower=0>[N_grp] sigma_log_u;
+  
+  // obs. error
+  vector<lower=0>[N_grp] sigma_log_e;
+  //real<lower=0> sigma_log_e;
+}
+transformed parameters {
+  // non-centered parameters
+  vector[N_pop] p0 = exp(log_mu_p[grp_pop] + log_p0_raw * sigma_log_p);
+  vector[N_pop] pK = exp(log_mu_p[grp_pop] + log_pK_raw * sigma_log_p);
+  vector[N_pop] tK = T + tK_raw * sigma_t;  // bound above T
+  vector[N_pop] tmax = tK .* tmax_raw;      // bound below tK
+  vector[N_plt] u = exp(log_u_raw .* sigma_log_u[grp_plt]);
+  vector[N_trt - N_grp] beta = exp(log_beta_raw .* sigma_log_b[grp_trt]);
+}
+model{
+  {
+    vector[N_trt] beta_ref;
+    vector[N] mu_plt;
+    vector[N] delta_gm;
+    vector[N] log_mu;
+    vector[N] delta_log_e;
+    
+    // Intercepts only
+    // Set reference class to one
+    beta_ref = append_row(rep_vector(1.0, N_grp), beta);
+    
+    // Multiplicative fixed and random effects
+    if(model_variant == 1) {
+      mu_plt = beta_curve(p0[pop] .* u[plt], 
+                          pK[pop] .* u[plt] .* beta_ref[trt], 
+                          tK[grp], tmax[grp], t, N);
+    }
+    else if (model_variant == 2){
+      mu_plt = beta_curve(p0[pop] .* u[plt], 
+                    pK[pop] .* u[plt], 
+                    tK[grp] .* beta_ref[trt],
+                    tmax[grp] .* beta_ref[trt], t, N);
+    }
+    else if (model_variant == 3) {
+      mu_plt = beta_curve(p0[pop] .* u[plt], 
+              pK[pop] .* u[plt] .* beta_ref[trt], 
+              tK[grp] .* beta_ref[trt],
+              tmax[grp] .* beta_ref[trt], t, N);
+    }
+    
+    // Stochastic model
+    // Adjustment for irregular meas.
+    delta_gm = exp(log(delta[grp]) .* gm);
+    
+    // Use intercept for first meas.
+    log_mu[m1] = log(delta_gm[m1] .* (p0[pop[m1]] .* u[plt[m1]]) + 
+                       (1 - delta_gm[m1]) .* mu_plt[m1]);
+    
+    // Lagged meas. for reamainder
+    log_mu[m] = log(delta_gm[m] .* y[m_m1] + (1 - delta_gm[m]) .* mu_plt[m]);
+    
+    
+    // Measurement model
+    // Uncertainty increases with time between meas.
+    delta_log_e = (1 - delta_gm) ./ (1 - delta[grp]) .* sigma_log_e[grp];
+    
+    // Abundance follows lognormal distn.
+    y[m_obs] ~ lognormal(log_mu[m_obs], delta_log_e[m_obs]);
+    
+    // Integrate out meas. below detection
+    //target += lognormal_lcdf(L[meas[m_mis]] | log_mu[m_mis], delta_log_e[m_mis]);
+  }
+  
+  // priors
+  log_p0_raw ~ normal(0, 1);
+  log_pK_raw ~ normal(0, 1);
+  log_mu_p ~ normal(0, 1);
+  sigma_log_p ~ normal(0, 1);
+  
+  tK_raw ~ normal(0, 1);
+  tmax_raw ~ beta(4, 4);
+  sigma_t ~ normal(0, 1);
+  
+  log_beta_raw ~ normal(0, 1);
+  sigma_log_b ~ normal(0, 1);
+  
+  delta ~ beta(4, 4);
+  
+  log_u_raw ~ normal(0, 1);
+  sigma_log_u ~ normal(0, 1);
+  sigma_log_e ~ normal(0, 1);
+}
---
+++ run_model.R
@@ -0,0 +1,200 @@
+#' utils
+
+run_models <- function(model = "f1", fields = 92, censored = T, variant = 1) {
+
+  output <- list(
+    model = model,
+    fields = fields,
+    censored = censored,
+    model_file = switch(model,
+      f1 = "cc/f1/field_mean1.stan",
+      f1N = "cc/f1/field_mean1N.stan",
+      f2 = "cc/f2/field_mean2.stan",
+      f2N = "cc/f2/field_mean2N.stan",
+      f3 = "cc/f3/field_mean3.stan"),
+    data_list = format_data(fields)
+  )
+  
+  output$data_list$model_variant = variant
+  output$data_list$cen = if_else(censored, 1, 0)
+  
+  output$fit <- stan(file = output$model_file,
+              data =output$data_list,
+              chains = 4,
+              iter = 300)
+  
+  censored <- switch(
+    T = "censored",
+    F = "uncensored"
+  )
+
+  output$ouput_file <- paste0(gsub("\\.stan", "", output$model_file), 
+                       censored, "v", variant, ".fit.Rdata")
+  
+  save(file = output$ouput_file, output)
+  message("Model saved")
+  
+  str(output, max.level = 1)
+  return(output)
+}
+
+
+format_data <- function(fields) {
+
+  y <- get_y(fields, quietly = T)
+  L <- get_L(y)
+  x <- get_x(y)
+  t <- get_t(x)
+  
+  message("\nStan data structure")
+  data_list <- list(
+    T = max(t$t),
+    N = nrow(x),
+    N_grp = max(x$grp),
+    N_trt = max(x$trt),
+    N_plt = max(x$plt),
+    N_pop = max(x$pop),
+    N_meas = max(x$meas),
+    N_mis = sum(x$mis),
+    t = t$t,
+    y = x$abun_std,
+    gm = t$gm,
+    L = L$min,
+    m1 = t$m[t$m == t$m1],
+    m = t$m[t$m != t$m1],
+    m_m1 = t$mm1[t$m != t$m1],
+    m_obs = t$m[t$mis == 0],
+    m_mis = t$m[t$mis == 1],
+    grp = x$grp,
+    grp_plt = nested(x$plt, x$grp),
+    grp_pop = nested(x$pop, x$grp),
+    grp_trt = nested(x$trt[x$trt > max(x$grp)], 
+                     x$grp[x$trt > max(x$grp)]),
+    trt = x$trt,
+    plt = x$plt,
+    pop = x$pop,
+    meas = x$meas
+  )
+
+  str(data_list)
+  
+  return(data_list)
+}
+
+load_data <- function(fields) {
+  # CC chronosequence + restoration experiment.
+  # Chronosequence plots have burned and unburned treatments.
+  # Restoration has 7 different contrasts, including negative controls.
+  # Consider C- same as R-, ignoring quadrat level differences.
+  plots <- read_csv("../data/CC_plot_data.csv", guess_max = 1e4) %>%
+    mutate(rest = case_when(burned == 0 ~ "R-",
+                            burned == 1 ~ "RB-",
+                            T ~ restoration93),
+           rest = factor(rest, levels = c("R-", "RB-", "C-", "C+",
+                                          "B", "HB", "BR", "BRN", "HBR")),
+           treat = factor(treatment99, levels = c("C", "N", "B", "N/B"))) %>%
+    select(plot_id, field, block, rest, yearabandoned, quad_id, treat) %>%
+    distinct()
+  
+  # Select three fields of similar abandonment age. 
+  # Bray-Curtis says 42 & 53 most similar of all CC fields to C- plots in 1993. 
+  # Group by origin and functional group. 
+  # Note: Poa pratensis considered 'Introduced' in this study as origin 
+  #       unknown but a weed in Minnesota. 
+  dat <- read_csv("../data/CC_species_data.csv", 
+                  col_types = "ciiccdddddcccccccc") %>%
+    select(-count, -pres) %>%
+    gather(meas, val, biomass:pcover) %>%
+    filter(val > 0) %>%
+    left_join(plots) %>%
+    filter(field %in% fields) %>%
+    filter(!grepl("ground|litter", species_id)) %>%
+    mutate(origin = if_else(sp == "Poa_pra", "Introduced", origin)) %>%
+    mutate(group = case_when(lifeform == "Grass" & 
+                               origin == "Introduced" ~ "Introduced grasses",
+                             lifeform == "Grass" &
+                               origin == "Native" ~ "Native grasses",
+                             func_grp == "LF" &
+                               origin == "Introduced" ~ "Introduced forbs",
+                             func_grp == "F" &
+                               origin == "Introduced" ~ "Introduced forbs",
+                             func_grp == "LF" & origin == "Native" ~ "Native forbs",
+                             func_grp == "F" & origin == "Native" ~ "Native forbs",
+                             func_grp == "W" ~ "Other",
+                             T ~ "Other")) %>%
+    mutate(group = factor(group, 
+                          levels = c("Introduced grasses", 
+                                     "Native grasses",
+                                     "Introduced forbs",
+                                     "Native forbs", 
+                                     "Other")))
+  
+}
+
+get_y <- function(fields, quietly = F) {
+  withCallingHandlers(
+    dat <- load_data(fields),
+    message = function(message){if(quietly) invokeRestart("muffleMessage")}
+  )
+  
+  # Aggregte groups at plot level in each year.
+  # Measurement techniques vary significantly between surveys,
+  # Standardise by meaurement type and year to maintain relative abundance 
+  # without converting to proportions. 
+  # Add zeros to plots where no abundance observed.
+  
+  message("Generating data")
+  y <- group_by(dat, field, year, group) %>%
+    group_by(field, year, plot_id, meas, rest, ab = yearabandoned, group) %>%
+    summarise(abun = sum(val)) %>%
+    complete(group, fill = list(abun = 0)) %>%
+    group_by(meas, year) %>%
+    mutate(sd = sd(abun),
+           abun_std = abun / sd) %>%
+    ungroup()
+}
+
+get_L <- function(y) {
+  # Find detection limits of each survey
+  L <- group_by(y, meas, year) %>%
+    summarise(min = min(abun_std[abun_std > 0])) %>%
+    ungroup() %>%
+    mutate(meas = as.numeric(factor(paste(year, meas)))) %>%
+    arrange(year)
+}
+
+get_x <- function(y) {
+  # Generate unique IDs for each group in each plot, treatment.
+  # Reference classes rely on R-/C- being first factor level.
+  # Generate ID for each survey, indicator for non-detections.
+  x <- y %>% 
+    mutate_at(vars(rest), ~ fct_recode(., `R-` = "C-")) %>%
+    mutate(grp = as.numeric(factor(group)),
+           plt = as.numeric(factor(paste(as.numeric(group), plot_id))),
+           pop = as.numeric(factor(paste(as.numeric(group), field))),
+           trt = grp + (as.numeric(rest) - 1) * max(grp),
+           meas = as.numeric(factor(paste(year, meas))),
+           mis = if_else(abun_std == 0, 1, 0)) %>%
+    arrange(plt, year)
+}
+
+get_t <- function(x) {
+  # Calculate time since abandonment, time between surveys for each plot.
+  # Generate indices for first, lagged measurements
+  t = select(x, ab, year, plt, grp, mis) %>%
+    mutate(t = year - ab,
+           m = row_number()) %>%
+    group_by(plt) %>%
+    mutate(m1 = min(m),
+           mm1 = lag(m, default = 0),
+           gm = year - lag(year, default = unique(ab))) %>%
+    ungroup() %>%
+    arrange(plt, year)
+  
+}
+
+nested <- function(x, y){
+  nestbl <- table(factor(x), factor(y))
+  idx <- unname(apply(nestbl, 1, function(x) which(x > 0)))
+  return(idx)
+}
